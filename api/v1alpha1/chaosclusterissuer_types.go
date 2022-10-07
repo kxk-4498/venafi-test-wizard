bad6fccee62ea5416947ff24ba6d310931da49aa
kxk-4498 <kaustubk@LAPTOP-D199TF5S.localdomain>
2022-10-06 17:50:42 -0700

    made changes to api folder and controller folder

---
+++ api/v1alpha1/chaosclusterissuer_types.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2022 CMU-SV.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// ClusterIssuer is the Schema for the clusterissuers API
+type ChaosClusterIssuer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ChaosIssuerSpec   `json:"spec,omitempty"`
+	Status ChaosIssuerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ClusterIssuerList contains a list of ClusterIssuer
+type ChaosClusterIssuerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ChaosClusterIssuer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ChaosClusterIssuer{}, &ChaosClusterIssuerList{})
+}
---
+++ controllers/chaosissuer_controller.go
@@ -0,0 +1,148 @@
+/*
+Copyright 2022 CMU-SV.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"github.com/kxk-4498/Venafi-test-wizard/issuer"
+
+	api "github.com/kxk-4498/Venafi-test-wizard/api/v1alpha1"
+	issuerutil "github.com/kxk-4498/Venafi-test-wizard/util"
+)
+
+const (
+	issuerReadyConditionReason = "certmanager.IssuerController.Reconcile"
+	defaultHealthCheckInterval = time.Minute
+)
+
+var (
+	errGetAuthSecret        = errors.New("failed to get Secret containing Issuer credentials")
+	errHealthCheckerBuilder = errors.New("failed to build the healthchecker")
+	errHealthCheckerCheck   = errors.New("healthcheck failed")
+)
+
+// IssuerReconciler reconciles a Issuer object
+type IssuerReconciler struct {
+	client.Client
+	Kind                     string
+	Scheme                   *runtime.Scheme
+	ClusterResourceNamespace string
+	HealthCheckerBuilder     issuer.HealthCheckerBuilder
+}
+
+// +kubebuilder:rbac:groups=certmanager.chaos.ch,resources=issuers;clusterissuers,verbs=get;list;watch
+// +kubebuilder:rbac:groups=certmanager.chaos.ch,resources=issuers/status;clusterissuers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+
+func (r *IssuerReconciler) newIssuer() (client.Object, error) {
+	issuerGVK := api.GroupVersion.WithKind(r.Kind)
+	ro, err := r.Scheme.New(issuerGVK)
+	if err != nil {
+		return nil, err
+	}
+	return ro.(client.Object), nil
+}
+
+func (r *IssuerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+	log := ctrl.LoggerFrom(ctx)
+
+	issuer, err := r.newIssuer()
+	if err != nil {
+		log.Error(err, "Unrecognised issuer type")
+		return ctrl.Result{}, nil
+	}
+	if err := r.Get(ctx, req.NamespacedName, issuer); err != nil {
+		if err := client.IgnoreNotFound(err); err != nil {
+			return ctrl.Result{}, fmt.Errorf("unexpected get error: %v", err)
+		}
+		log.Info("Not found. Ignoring.")
+		return ctrl.Result{}, nil
+	}
+
+	issuerSpec, issuerStatus, err := issuerutil.GetSpecAndStatus(issuer)
+	if err != nil {
+		log.Error(err, "Unexpected error while getting issuer spec and status. Not retrying.")
+		return ctrl.Result{}, nil
+	}
+
+	// Always attempt to update the Ready condition
+	defer func() {
+		if err != nil {
+			issuerutil.SetReadyCondition(issuerStatus, api.ConditionFalse, issuerReadyConditionReason, err.Error())
+		}
+		if updateErr := r.Status().Update(ctx, issuer); updateErr != nil {
+			err = utilerrors.NewAggregate([]error{err, updateErr})
+			result = ctrl.Result{}
+		}
+	}()
+
+	if ready := issuerutil.GetReadyCondition(issuerStatus); ready == nil {
+		issuerutil.SetReadyCondition(issuerStatus, api.ConditionUnknown, issuerReadyConditionReason, "First seen")
+		return ctrl.Result{}, nil
+	}
+
+	secretName := types.NamespacedName{
+		Name: issuerSpec.AuthSecretName,
+	}
+
+	switch issuer.(type) {
+	case *api.ChaosIssuer:
+		secretName.Namespace = req.Namespace
+	case *api.ChaosClusterIssuer:
+		secretName.Namespace = r.ClusterResourceNamespace
+	default:
+		log.Error(fmt.Errorf("unexpected issuer type: %t", issuer), "Not retrying.")
+		return ctrl.Result{}, nil
+	}
+
+	var secret corev1.Secret
+	if err := r.Get(ctx, secretName, &secret); err != nil {
+		return ctrl.Result{}, fmt.Errorf("%w, secret name: %s, reason: %v", errGetAuthSecret, secretName, err)
+	}
+
+	checker, err := r.HealthCheckerBuilder(issuerSpec, secret.Data)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("%w: %v", errHealthCheckerBuilder, err)
+	}
+
+	if err := checker.Check(); err != nil {
+		return ctrl.Result{}, fmt.Errorf("%w: %v", errHealthCheckerCheck, err)
+	}
+
+	issuerutil.SetReadyCondition(issuerStatus, api.ConditionTrue, issuerReadyConditionReason, "Success")
+	return ctrl.Result{RequeueAfter: defaultHealthCheckInterval}, nil
+}
+
+func (r *IssuerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	issuerType, err := r.newIssuer()
+	if err != nil {
+		return err
+	}
+	return ctrl.NewControllerManagedBy(mgr).
+		For(issuerType).
+		Complete(r)
+}
