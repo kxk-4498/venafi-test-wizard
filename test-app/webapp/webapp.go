2678653939161f6cd0402147e89cd3bef47a863b
Sihan Ding <sd3rf@virginia.edu>
2022-11-09 23:30:56 -0800

    Finished app

--- test-app/webapp/webapp.go
+++ test-app/webapp/webapp.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Begin")
+
 	// handle `/` route
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Hello World!")
 	})
+
 	// run server on port "9000"
 	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
 
