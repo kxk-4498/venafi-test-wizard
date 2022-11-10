1f1865379c059c6b035e6d04092fa5527f1dde9e
Sihan Ding <sd3rf@virginia.edu>
2022-11-09 21:47:33 -0800

    Add manual secretfiles

--- test-app/webapp/web.go
+++ test-app/webapp/web.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-
-	// generate a `Certificate` struct
-	cert, _ := tls.LoadX509KeyPair("localhost.crt", "localhost.key")
-
-	// create a custom server with `TLSConfig`
-	s := &http.Server{
-		Addr:    ":9000",
-		Handler: nil, // use `http.DefaultServeMux`
-		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{cert},
-		},
-	}
-
+	fmt.Println("Begin")
 	// handle `/` route
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello Custom World!")
+		fmt.Fprint(res, "Hello World!")
 	})
-
 	// run server on port "9000"
-	log.Fatal(s.ListenAndServeTLS("", ""))
+	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
 
 }
